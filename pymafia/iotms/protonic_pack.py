54271a61105bc7f6d74aed689c37913a3768265b
David <MrFizzyBubbs@protonmail.com>
2022-02-15 12:04:26 -0600

    Add protonic ghosts

--- pymafia/iotms/protonic_pack.py
+++ pymafia/iotms/protonic_pack.py
@@ -1,9 +1,25 @@
 from pymafia import ash
-from pymafia.datatypes import Item, Location
+from pymafia.datatypes import Item, Location, Monster
 from pymafia.utils import get_property
 from pymafia.utils import have as _have
 
 item = Item("protonic accelerator pack")
+
+ghosts = [
+    Monster("boneless blobghost"),
+    Monster("Emily Koops, a spooky lime"),
+    Monster("The ghost of Ebenoozer Screege"),
+    Monster("The ghost of Jim Unfortunato"),
+    Monster("The ghost of Lord Montague Spookyraven"),
+    Monster("the ghost of Monsieur Baguelle"),
+    Monster("the ghost of Oily McBindle"),
+    Monster("The ghost of Richard Cockingham"),
+    Monster("the ghost of Sam McGee"),
+    Monster('the ghost of Vanillica "Trashblossom" Gorton'),
+    Monster("The ghost of Waldo the Carpathian"),
+    Monster("The Headless Horseman"),
+    Monster("The Icewoman"),
+]
 
 
 def have():
