6f595e2444588543f9754ed7bac23b90c4421342
MrFizzyBubbs <MrFizzyBubbs@protonmail.com>
2022-09-02 13:21:54 -0500

    Privatize datatype files

--- src/pymafia/datatypes/__init__.py
+++ src/pymafia/datatypes/__init__.py
@@ -1,16 +1,16 @@
-from .bounty import Bounty
-from .class_ import Class
-from .coinmaster import Coinmaster
-from .effect import Effect, EffectQuality
-from .element import Element
-from .familiar import Familiar
-from .item import CandyType, Item, ItemQuality
-from .location import Location
-from .monster import Monster
-from .phylum import Phylum
-from .servant import Servant
-from .skill import Skill
-from .slot import Slot
-from .stat import Stat
-from .thrall import Thrall
-from .vykea import Vykea
+from ._bounty import Bounty
+from ._class import Class
+from ._coinmaster import Coinmaster
+from ._effect import Effect, EffectQuality
+from ._element import Element
+from ._familiar import Familiar
+from ._item import CandyType, Item, ItemQuality
+from ._location import Location
+from ._monster import Monster
+from ._phylum import Phylum
+from ._servant import Servant
+from ._skill import Skill
+from ._slot import Slot
+from ._stat import Stat
+from ._thrall import Thrall
+from ._vykea import Vykea
