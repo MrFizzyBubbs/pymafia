5abf580beb3ba4860a19fe848409fa52ab3a9ddc
MrFizzyBubbs <MrFizzyBubbs@protonmail.com>
2023-04-11 19:32:26 -0500

    Add __all__ to every submodule

--- src/pymafia/iotms/__init__.py
+++ src/pymafia/iotms/__init__.py
@@ -1,3 +1,20 @@
+__all__ = [
+    "barrel_god",
+    "boxing_daycare",
+    "cartography",
+    "cosplay_saber",
+    "crimbo_shrub",
+    "god_lobster",
+    "love_tunnel",
+    "mumming_trunk",
+    "pantogram",
+    "protonic_pack",
+    "snojo",
+    "songboom",
+    "stomping_boots",
+    "witchess",
+]
+
 from pymafia.iotms import (
     barrel_god,
     boxing_daycare,
--- src/pymafia/kolmafia/__init__.py
+++ src/pymafia/kolmafia/__init__.py
@@ -1,4 +1,4 @@
+__all__ = ["KoLmafiaError", "km"]
+
 from pymafia.kolmafia.kolmafia import km
 from pymafia.kolmafia.patch import KoLmafiaError
-
-__all__ = ["KoLmafiaError", "km"]
