9e14cfe53907855c60f4cb2aed8714f56e4ee1a3
MrFizzyBubbs <MrFizzyBubbs@protonmail.com>
2023-07-28 10:00:19 -0500

    Test Matcher and record conversions

--- tests/test_ash.py
+++ tests/test_ash.py
@@ -1,8 +1,12 @@
+import jpype
 import pytest
 
 from pymafia.ash.conversion import from_java, to_java
-from pymafia.datatypes import SPECIAL_DATATYPES
+from pymafia.ash.library import git_info
+from pymafia.datatypes import SPECIAL_DATATYPES, Matcher
 from pymafia.kolmafia import km
+
+JPattern = jpype.JClass("java.util.regex.Pattern")
 
 
 @pytest.mark.parametrize(
@@ -16,3 +20,18 @@ def test_java_conversion(value):
 
 def test_void_java_conversion():
     assert from_java(km.Value()) is None
+
+
+def test_matcher_java_conversion():
+    matcher = Matcher(JPattern.compile("(\\d)").matcher("0"))
+    assert from_java(to_java(matcher)).__wrapped__ == matcher.__wrapped__
+
+
+def test_record_java_conversion():
+    assert git_info("") == {
+        "url": "",
+        "branch": "",
+        "commit": "",
+        "last_changed_author": "",
+        "last_changed_date": "",
+    }
